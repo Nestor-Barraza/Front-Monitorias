8bf1c93bf2587f9669bb6493821f4df6fa82ab18
Nestor <nestorbarrazaotalvarez@hotmail.com>
2020-10-05 01:53:42 -0500

    cambios visuales

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import "./home.css"
 const Home = () => (
   <div style={{ textAlign: "center" }}>
-    <h1 style={{color: "white", marginTop: "20%"}}>Univeridad X</h1>
+    <h1 id='titulo' style={{color: "white",marginTop: "20%"}}>Univeridad X</h1>
+    <h1 id='titulo'  style={{color: "cyan", fontSize: "12px"}}>Una mirada tecnológica</h1>
+    <div id='img'>
+      gffgf
+    </div>
   </div>
 );
 
